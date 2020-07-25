9e1dd3300fd42e41dec7648be8f2d960b8aa448c
SergSmith64 <sergsmith2008@yandex.ru>
2020-07-25 18:40:51 +0400

    feat: let's update the GET method to get data from the remote server

--- work/src/App.js
+++ work/src/App.js
@@ -4,6 +4,9 @@ import React from "react";
 // import User from './User';
 
 // __ я добавил : __
+// __ ТЕПЕРЬ ОШИБКА: __ ERROR !!! ________________________
+// 'photoJessica' is defined but never used no-unused-vars
+// __ так как инфа и картинка грузится с сервера _________
 import photoJessica from './Jessica.png';
 
 // __ теперь загрузим "реальные" данные с помощью асинхронного запроса __
@@ -37,11 +40,34 @@ class App extends React.Component {
       <User isLoading={isLoading} name={name} avatar={avatar} email={email} />
     );
   }
-  // __ добавляем __
+
+
+  // __ И под конец, давайте обновим метод GET, чтобы получить данные с удаленного сервера __
+  // __ let's update the GET method to get data from the remote server ______________________
   async componentDidMount() {
     // Load async data.
-    // Update state with new data.
-    // Re-render our component.
+    let userData = await API.get('/', {
+      params: {
+        results: 1,
+        inc: 'name,email,picture'
+      }
+    });
+    // __ ПАРСИМ РЕЗУЛЬТАТЫ __
+    userData = userData.data.results[0];
+
+    // __ ОБНОВЛЯЕМ СТЕЙТ И РЕРЕНДЕРИМ НАШ КОМПОНЕНТ __
+    const name = `${userData.name.first} ${userData.name.last}`;
+    const avatar = userData.picture.large;
+    const email = userData.email;
+
+    this.setState({
+      ...this.state, ...{
+        isLoading: false,
+        name,
+        avatar,
+        email
+      }
+    });
   }
 }
 export default App;
