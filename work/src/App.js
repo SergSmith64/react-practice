4386ec15bca3e2a070649d1cc6c0f5edb0691a09
SergSmith64 <sergsmith2008@yandex.ru>
2020-07-25 10:10:00 +0400

    init: add axios & shards-react

---
+++ work/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Привет! <code>src/App.js</code> Сохрани!
+        </p>
+        <a className="App-link" href="#" target="_blank">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
