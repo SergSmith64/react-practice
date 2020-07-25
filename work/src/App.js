f9054ad79935c071170ad9c77206db73d66aeee0
SergSmith64 <sergsmith2008@yandex.ru>
2020-07-25 10:46:57 +0400

    feat: add photo in card

--- work/src/App.js
+++ work/src/App.js
@@ -1,21 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// __ добавил : __
+import User from './User';
+import photoJessica from './Jessica.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Привет! <code>src/App.js</code> Сохрани!
-        </p>
-        <a className="App-link" href="#" target="_blank">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    // return <User name="Jessica Doe" avatar="..." email="hello@jessica.com" />;
+
+    return <User name="Jessica Doe" avatar={photoJessica} email="hello@jessica.com" />;
+  }
 }
-
 export default App;
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Привет! <code>src/App.js</code> Сохрани!
+//         </p>
+//         <a className="App-link" href="#" target="_blank">
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+// export default App;
